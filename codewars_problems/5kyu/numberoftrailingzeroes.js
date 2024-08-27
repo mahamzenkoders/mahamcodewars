0662b880682356099d88c94a3ae398cd7bd21105
Maham Sultana <mahamzenkoders@gmail.com>
2024-08-28 02:44:24 +0500

    Update numberoftrailingzeroes.js

--- codewars_problems/5kyu/numberoftrailingzeroes.js
+++ codewars_problems/5kyu/numberoftrailingzeroes.js
@@ -1,3 +1,9 @@
+// Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+// N! = 1 * 2 * 3 *  ... * N
+
+// Be careful 1000! has 2568 digits...
+
 function zeros(n) {
     let count = 0;
     while (n >= 5) {
@@ -8,5 +14,4 @@ function zeros(n) {
 }
 
 console.log(zeros(6)); 
-console.log(zeros(10)); 
 console.log(zeros(30));
